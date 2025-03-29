53a4bf7209db9aee69150e65f5127b3746ee3b9f
Claire Chen <cichen@cpp.edu>
2025-03-28 17:08:00 -0700

    random-number

--- magicSquare.c
+++ magicSquare.c
@@ -8,17 +8,24 @@ int isMagicSquare(int square[SIZE][SIZE])
     int magicSum = 15;
     int num[10] = {0}; // To track numbers 1-9
 
+    printf("=== Checking square in isMagicSquare ===\n");
+    printSquare(square);
+
+    //check if all numbers from 1 to 9 are present
     for(int i = 0; i < SIZE; i++)
     {
         for(int j = 0; j < SIZE; j++)
         {
             int value = square[i][j];
-            if(value < 1 || value > 9 || num[value])
+            if(value < 1 || value > 9)
+                return 0;
+            if(num[value])
                 return 0;
             num[value] = 1;
         }
     }
 
+    //check rows and columns sums equals magic sum
     for(int i = 0; i < SIZE; i++)
     {
         int rowSum = 0, colSum = 0;
@@ -31,10 +38,37 @@ int isMagicSquare(int square[SIZE][SIZE])
             return 0;
     }
 
+    //check diagonals sums equals magic sum
     int diag1 = square[0][0] + square[1][1] + square [2][2];
     int diag2 = square[0][2] + square[1][1] + square[2][0];
     if(diag1 != magicSum || diag2 != magicSum)
         return 0;
 
     return 1;
+}
+
+void shuffle(int *arr, int size)
+{
+    for(int i = size - 1; i > 0; i--)
+    {
+        int j = rand() % (i + 1);
+        int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+    }
+}
+
+void fillSquare(int square[SIZE][SIZE], int *flat)
+{
+    for(int i = 0; i < SIZE * SIZE; i++)
+        square[i / SIZE][i % SIZE] = flat[i];
+}
+
+void printSquare(int square[SIZE][SIZE])
+{
+    for(int i = 0; i < SIZE; i++)
+    {
+        printf("[");
+        for(int j = 0; j < SIZE; j++)
+            printf("%d%c", square[i][j], (j < SIZE - 1) ? ' ' : ']');
+        printf("\n");
+    }
 }
--- magicSquareMain.c
+++ magicSquareMain.c
@@ -5,6 +5,27 @@
 
 int main()
 {
+    //random square
+    srand(time(NULL));
+    int square[SIZE][SIZE];
+    int flat[9], count = 0;
+
+    for(int i = 0; i < 9; i++) 
+    {
+        flat[i] = i + 1;
+    }
+
+    do
+    {
+        shuffle(flat, 9);
+        fillSquare(square, flat);
+        count++;
+    }while (!isMagicSquare(square));
+
+    printf("Found a magic square after %d attempts:\n", count);
+    printSquare(square);
+
+    //test cases
     int validSquare[SIZE][SIZE] = {
         {8, 1, 6},
         {3, 5, 7},
